72d5241850449e04c05f76974d19479ee18ce734
Tim Butters <tim.butters@sabisu.co>
2015-12-22 09:17:07 +0000

    Fixed python style (pep 8)

--- VirtualPlant/VirtualPlant.py
+++ VirtualPlant/VirtualPlant.py
@@ -29,35 +29,37 @@ class Pump:
 class Furnace:
     """A simple furnace model"""
     # stef_boltz_const = 5.67037E-8
-    # emissivity = 0.9 # Between 0 and 1
-    length = 20 # metres
-    density_h20 = 0.804 # kg/m3
-    furnace_flow = 30 # m3/h
-    heat_capacity_h20 = 4181.3 # J/kg/K - Of the exchanger substance (water)
-    heat_capacity_pro = 2191.0 # J/kg/K - Of process substance (Methane)
-    density_pro = 0.668 # kg/m3 Methane
+    # emissivity = 0.9  # Between 0 and 1
+    length = 20  # metres
+    density_h20 = 0.804  # kg/m3
+    furnace_flow = 30  # m3/h
+    heat_capacity_h20 = 4181.3  # J/kg/K - Of the exchanger substance (water)
+    heat_capacity_pro = 2191.0  # J/kg/K - Of process substance (Methane)
+    density_pro = 0.668  # kg/m3 Methane
+
     def __init__(self, temperature=500):
         self.temperature = temperature
-        
+
     def print(self):
         return "-{F}-"
 
     def advance(self, flow_in, temp_in):
-        temp_in = temp_in + 273.15 # Convert from degC to K
+        temp_in = temp_in + 273.15  # Convert from degC to K
         furnace_temp = self.temperature + 273.15
-        
+
         # Mass per unit time converting m3/h to m3/sec
         mass_per_unit_time_h20 = (self.furnace_flow/3600.0) * self.density_h20
         mass_per_unit_time_pro = (flow_in/3600.0) * self.density_pro
 
-        gamma = 1 # "Thermal Connection Constant": Don't know what this should be?? Units are 1/m
+# "Thermal Connection Constant" Don't know what this should be?? Units are 1/m
+        gamma = 1
         k1 = gamma/(self.heat_capacity_h20*mass_per_unit_time_h20)
         k2 = gamma/(self.heat_capacity_pro*mass_per_unit_time_pro)
         k = k1 + k2
 
         temp_out = ((temp_in*k1 + furnace_temp*k2)/k +
-                   (((temp_in - furnace_temp)*k2)/k) * np.exp(-k*self.length))
- 
+                    (((temp_in - furnace_temp)*k2)/k) * np.exp(-k*self.length))
+
         # Convert K to degC
         return flow_in, (temp_out - 273.15)
 
@@ -86,7 +88,8 @@ class Plant:
             variables.append([outflow, outtemp])
 
             if display:
-                print("{0}/{1} {2} ".format(inflow, intemp, asset.print()), end="")
+                print("{0}/{1} {2} ".format(inflow, intemp,
+                                            asset.print()), end="")
             inflow = outflow
             intemp = outtemp
 
